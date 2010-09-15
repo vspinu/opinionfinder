58d87f7959fa0ec5b2ce056b47b2567fab5aa132
alexander.p.conrad <alexander.p.conrad@6ef2f8d6-1717-1a27-9042-8b16f1f5fe38>
2010-09-15 05:19:51 +0000

    alex is ~2/3rds done with the new featurefinder. Also, uploading featurefiles, example datasets, etc

--- trunk/OpinionFinder2/src/opin/main/RunOpinionFinder.java
+++ trunk/OpinionFinder2/src/opin/main/RunOpinionFinder.java
@@ -14,10 +14,53 @@ public class RunOpinionFinder {
 
         // read configuration files
 
-        // module 1: preprocessing
+        // *** modules 1 and 2: preprocessing and stemming ***
 
-        
+        // remove html / xml markup (opin.preprocessor.TextExtract.java)
 
+        // replace non-ascii chars and escape code with ascii approximations
+        //  (opin.preprocessor.EscapeCodeReplacer.java)
+
+        // sentence splitting, tokenization,l pos tagging, and lemmatization
+        //  (opin.preprocessor.PreProcess.java)
+
+
+        // *** module 3: unique feature finder ***
+        // this component is no longer with us; was only needed for
+        //  intensity classification
+
+        // *** module 4: feature finder ***
+
+        // use ngrams to identify subjectivity clues
+        //  (opin.featurefinder.FeatFind.java)
+
+
+        // *** module 5: shallow parser ***
+
+        // we are no longer using sundance
+        // leave annotator in perl for now?
+
+
+        // *** module 6: source finder ***
+        // might not need this / leave in perl?
+
+
+        // *** module 7: polarity classifier ***
+
+        // leave this in perl for now?
+
+
+        // *** module 8: direct subjective and speech event classifier ***
+
+
+        // *** module 9: subjective sentence classifier ***
+
+
+        // *** module 10: sgml markup ***
+
+
+        // *** module 11: cleanup ***
+        // we don't need this anymore, correct?
 
 
 
