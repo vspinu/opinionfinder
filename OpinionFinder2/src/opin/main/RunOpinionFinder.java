c4610614fb2699198ae4ed1ec1a34cf8019015b6
alexander.p.conrad <alexander.p.conrad@6ef2f8d6-1717-1a27-9042-8b16f1f5fe38>
2010-12-20 20:30:18 +0000

    opinfinder: alex

--- OpinionFinder2/src/opin/main/RunOpinionFinder.java
+++ OpinionFinder2/src/opin/main/RunOpinionFinder.java
@@ -1,16 +1,22 @@
 package opin.main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 import org.apache.commons.cli.*;
 import org.apache.commons.io.FileUtils;
 
+import opin.entity.*;
 import opin.util.*;
+import opin.preprocessor.*;
+import opin.featurefinder.*;
 
 /**
  * This class executes the main OpinionFinder pipeline. Note: lib/ must be on
@@ -20,8 +26,10 @@ import opin.util.*;
  */
 public class RunOpinionFinder {
 
+    public static String autoDoclist;
+    public static Corpus corpus;
+
     public static String sep;
-    
     public static String osName;
     public static boolean windows;
     public static boolean win95;
@@ -46,6 +54,7 @@ public class RunOpinionFinder {
         String featfileList = sep+"config"+sep+"featurefilelist";
         String name = "default";
         String databasePath = "";
+        String modelPath = "models"+sep+"left3words-wsj-0-18.tagger";
 
         Options options = new Options();
 
@@ -71,12 +80,18 @@ public class RunOpinionFinder {
                 .hasArg()
                 .withDescription("path to root database directory")
                 .create("db");
+        Option modelOption = OptionBuilder.withArgName("modelpath")
+                .hasArg()
+                .withDescription("path to model for stanford parser")
+                .create("m");
 
         options.addOption(helpOption);
         options.addOption(encodingOption);
         options.addOption(doclistOption);
         options.addOption(featfileOption);
         options.addOption(databaseOption);
+        options.addOption(nameOption);
+        options.addOption(modelOption);
 
         if (args.length == 0) {
             HelpFormatter formatter = new HelpFormatter();
@@ -112,6 +127,9 @@ public class RunOpinionFinder {
             if (cmd.hasOption("db")) {
                 databasePath = cmd.getOptionValue("db");
             }
+            if (cmd.hasOption("m")) {
+                modelPath = cmd.getOptionValue("m");
+            }
 
 
         } catch (ParseException e) {
@@ -123,17 +141,25 @@ public class RunOpinionFinder {
         // read configuration files
 
         // copy doclist files to database, if they're not there already
+        StringBuilder newDoclist = new StringBuilder();
+
         try {
+
             Scanner doclistContents = new Scanner(new BufferedReader(new FileReader(doclistPath)));
 
             while (doclistContents.hasNextLine()) {
                 File source = new File(doclistContents.nextLine());
                 File destDir = new File(databasePath+sep+"docs"+sep+name);
                 if (!source.getCanonicalPath().equals(databasePath+sep+"docs"+sep+name+sep+source.getName())) {
+                    if (!destDir.exists()) {
+                        destDir.mkdirs();
+                    }
                     FileUtils.copyFileToDirectory(source, destDir);
                 }
-
+                newDoclist.append(databasePath+sep+"docs"+sep+name+sep+source.getName()+"\n");
             }
+
+            doclistContents.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -145,30 +171,71 @@ public class RunOpinionFinder {
             System.exit(1);
         }
 
+        autoDoclist = "";
+        try {
+
+            File autoAnns = new File(databasePath+sep+"auto_anns");
+            if (!autoAnns.exists()) {
+                autoAnns.mkdirs();
+            }
+            autoDoclist = databasePath+sep+"auto_anns"+sep+name+"doclist";
+            BufferedWriter newDoclistWriter = new BufferedWriter(new FileWriter(new File(autoDoclist)));
+            newDoclistWriter.append(newDoclistWriter.toString());
+            newDoclistWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error while building doclist, exiting...");
+            System.exit(1);
+        }
+
         // build corpus from copied docs
+        corpus = new Corpus(new File(autoDoclist));
 
         // *** modules 1 and 2: preprocessing and stemming ***
-
-        // remove html / xml markup (opin.preprocessor.TextExtract.java)
-        
-
-        // replace non-ascii chars and escape code with ascii approximations
-        //  (opin.preprocessor.EscapeCodeReplacer.java)
+        ArrayList<File> docs = corpus.getDocs();
+        EscapeCodeReplacer.makeEscapeLists();
+        for (File doc : docs) {
+            // remove html / xml markup (opin.preprocessor.TextExtract.java)
+            TextExtract.removeMarkup(doc.getAbsolutePath(), doc.getAbsolutePath());
+            // replace non-ascii chars and escape code with ascii approximations
+            //  (opin.preprocessor.EscapeCodeReplacer.java)
+            EscapeCodeReplacer.replaceEscapes(doc.getAbsolutePath());
+        }
 
         // sentence splitting, tokenization,l pos tagging, and lemmatization
         //  (opin.preprocessor.PreProcess.java)
+        PreProcess preprocessor = new PreProcess(new File(modelPath), encoding);
+        preprocessor.process(corpus);
 
-
+        
         // *** module 3: unique feature finder ***
         // this component is no longer with us; was only needed for
         //  intensity classification
 
-        // *** module 4: feature finder ***
 
+        // *** module 4: feature finder ***
+        ArrayList<File> featFiles = new ArrayList<File>();
+        try {
+
+            Scanner featFileScanner = new Scanner(new BufferedReader(new FileReader(new File(featfileList))));
+            while (featFileScanner.hasNextLine()) {
+                featFiles.add(new File(featFileScanner.nextLine()));
+            }
+            featFileScanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error while reading feature file list, exiting...");
+            System.exit(1);
+        }
+        
         // use ngrams to identify subjectivity clues
         //  (opin.featurefinder.FeatFind.java)
+        FeatFind featureFinder = new FeatFind(featFiles, encoding);
+        featureFinder.process(corpus);
 
-
+        
         // *** module 5: shallow parser ***
 
         // we are no longer using sundance
